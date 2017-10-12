05b3f81b432bf1ba5e8a3a06702f104775e2c22e
kenny <kenny841126@gmail.com>
2017-10-12 23:54:49 +0800

    zz

--- kenny/index.js
+++ kenny/index.js
@@ -22,35 +22,18 @@ var bot = linebot({
   "channelAccessToken": "/0HWJ3EzlNXylQ3+tC3iDdHm95e+QOhpXKy0bYf49UknQ+qobarTauYCMku/0+xgkhPe6t2MYNnYl0/9KN8hxMdi1CEVuRSQTO9NvBSL9HSDK++01uu5o6SEchXL9fS4NKODAfuLcDCZGG07jse2iQdB04t89/1O/w1cDnyilFU="
 }); // 連接line，驗證
 
-_japan();
+// _japan();
 
 // bot.on('message', function(event) {
 //   if (event.message.type = 'text') {
-//     // var msg = event.message.text;
-	  
-// 	  request({
-// 	    url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
-// 	    method: "GET"
-// 	  }, function(error, response, body) {
-// 	    if (error || !body) {
-// 	      return;
-// 	    } else {
-// 	      var $ = cheerio.load(body);
-// 	      var target = $(".rate-content-cash text-right print_hide");
-// 	      console.log(target[0].children[0].data);
-// 	      jp = target[0].children[0].data;
-// 		  var msg = ('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
-// 		  	  //收到文字訊息時，直接把收到的訊息傳回去
-//     	  event.reply(msg).then(function(data) {
-//       		// 傳送訊息成功時，可在此寫程式碼 
-//       		console.log(msg);
-//     	  }).catch(function(error) {
-//       		// 傳送訊息失敗時，可在此寫程式碼 
-//       		console.log('錯誤產生，錯誤碼：'+error);
-//    		  });
-
-	      
-// 	    }
+//     var msg = event.message.text;
+//     event.reply(msg).then(function(data) {
+//       // success 
+//       console.log(msg);
+//     }).catch(function(error) {
+//       // error 
+//       console.log('error');
+//     });
 //   }
 // });
 
@@ -112,26 +95,26 @@ function _getJSON() {
 }	  
 
 
-function _japan() {
-  clearTimeout(timer2);
-  request({
-    url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
-    method: "GET"
-  }, function(error, response, body) {
-    if (error || !body) {
-      return;
-    } else {
-      var $ = cheerio.load(body);
-      var target = $(".rate-content-cash text-right print_hide");
-      console.log(target[0].children[0].data);
-      jp = target[0].children[0].data;
-	  if (jp > 10) {
-	  	bot.push('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
-      }
-      timer2 = setInterval(_japan, 10000);
-    }
-  });
-}
+// function _japan() {
+//   clearTimeout(timer2);
+//   request({
+//     url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
+//     method: "GET"
+//   }, function(error, response, body) {
+//     if (error || !body) {
+//       return;
+//     } else {
+//       var $ = cheerio.load(body);
+//       var target = $(".rate-content-cash text-right print_hide");
+//       console.log(target[0].children[0].data);
+//       jp = target[0].children[0].data;
+// 	  if (jp > 10) {
+// 	  	bot.push('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
+//       }
+//       timer2 = setInterval(_japan, 10000);
+//     }
+//   });
+// }
 
 
 
