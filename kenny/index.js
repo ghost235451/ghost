70dc85b296f719302e7ecbfb21edd0269b19ae9e
kenny <kenny841126@gmail.com>
2017-10-16 20:44:47 +0800

    zz

--- kenny/index.js
+++ kenny/index.js
@@ -24,6 +24,7 @@ bot.on('message', function(event) {
     var msg = event.message.text;
   //收到文字訊息時，直接把收到的訊息傳回去
     event.reply(msg).then(function(data) {
+    	_japan();
       // 傳送訊息成功時，可在此寫程式碼 
       console.log(msg);
     }).catch(function(error) {
@@ -98,6 +99,41 @@ var server = app.listen(process.env.PORT || 8080, function() {
 
 
 
+// var jp = function() {
+//   request({
+//     url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
+//     method: "GET"
+//   }, function(error, response, body) {
+//     if (error || !body) {
+//       return;
+//     }else{
+
+//     // 爬完網頁後要做的事情
+//         console.log(body);
+//     }
+//   });
+// };
+
+function _japan() {
+  // clearTimeout(timer2);
+  request({
+    url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
+    method: "GET"
+  }, function(error, response, body) {
+    if (error || !body) {
+      return;
+    } else {
+      var $ = cheerio.load(body);
+      var target = $(".rate-content-cash text-right print_hide");
+      console.log(target[0].children[0].data);
+      var jp = target[0].children[0].data;
+
+      bot.push('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
+
+      // timer2 = setInterval(_japan, 1000);
+    }
+  });
+}
 
 
 
