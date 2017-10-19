88d84bca3b0722c3abc7654e8d9df2179bbe4329
kenny <kenny841126@gmail.com>
2017-10-19 19:38:49 +0800

    zz

--- kenny/index.js
+++ kenny/index.js
@@ -101,30 +101,30 @@ function _getJSON() {
 
 
 
-function _japan() {
-  // clearTimeout(timer2);
-  request({
-    url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
-    method: "GET"
-  }, function(error, response, body) {
-    if (error || !body) {
-      return;
-    } else {
-      var $ = cheerio.load(body);
-      var target = $(".rate-content-sight.text-right.print_hide");
-      // console.log(target[14].children[0].data);
-      var jp = target[14].children[0].data;
-      var jp2 = target[0].children[0].data;
-      // if (jp > 0) {
-      	bot.on('message',function(event){
-      		event.reply('現在日幣匯率' + jp +'美金' +jp2);
-      	});
-        // bot.reply('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
-      // }
-      // timer2 = setInterval(_japan, 120000);
-    }
-  });
-}
+// function _japan() {
+//   // clearTimeout(timer2);
+//   request({
+//     url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
+//     method: "GET"
+//   }, function(error, response, body) {
+//     if (error || !body) {
+//       return;
+//     } else {
+//       var $ = cheerio.load(body);
+//       var target = $(".rate-content-sight.text-right.print_hide");
+//       // console.log(target[14].children[0].data);
+//       var jp = target[14].children[0].data;
+//       var jp2 = target[0].children[0].data;
+//       // if (jp > 0) {
+//       	bot.on('message',function(event){
+//       		event.reply('現在日幣匯率' + jp +'美金' +jp2);
+//       	});
+//         // bot.reply('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
+//       // }
+//       // timer2 = setInterval(_japan, 120000);
+//     }
+//   });
+// }
 
 // function _japan() {
 // 	bot.on('message', function(event) {
