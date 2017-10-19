7fea0594f2370e5fd165ffa58aafc4ff47d2fe8f
kenny <kenny841126@gmail.com>
2017-10-19 17:23:37 +0800

    qq

--- kenny/index.js
+++ kenny/index.js
@@ -22,8 +22,8 @@ var bot = linebot({
 
 bot.on('message', function(event) {
   if (event.message.type = 'text') {
+  	_japan();
     var msg = event.message.text;
-    var msg2 = _japan();
   //收到文字訊息時，直接把收到的訊息傳回去
     event.reply(msg).then(function(data) {
     	event.reply('...')
