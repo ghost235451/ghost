cc8543bfd912a9639b5cf5dec7822761f9971631
kenny <kenny841126@gmail.com>
2017-10-19 17:39:52 +0800

    qq

--- kenny/index.js
+++ kenny/index.js
@@ -19,22 +19,40 @@ var bot = linebot({
   "channelAccessToken": "/0HWJ3EzlNXylQ3+tC3iDdHm95e+QOhpXKy0bYf49UknQ+qobarTauYCMku/0+xgkhPe6t2MYNnYl0/9KN8hxMdi1CEVuRSQTO9NvBSL9HSDK++01uu5o6SEchXL9fS4NKODAfuLcDCZGG07jse2iQdB04t89/1O/w1cDnyilFU="
 }); // 連接line，驗證
 
-
 bot.on('message', function(event) {
   if (event.message.type = 'text') {
-  	_japan();
-    var msg = event.message.text;
-  //收到文字訊息時，直接把收到的訊息傳回去
-    event.reply(msg).then(function(data) {
-    	event.reply('...')
-      // 傳送訊息成功時，可在此寫程式碼 
-      console.log(msg);
-    }).catch(function(error) {
-      // 傳送訊息失敗時，可在此寫程式碼 
-      console.log('錯誤產生，錯誤碼：'+error);
-    });
+  	request({
+    url: "http://blog.infographics.tw",
+    method: "GET"
+  }, function(e,r,b) {
+    if(e || !b) { return; }
+    var $ = cheerio.load(b);
+    var result = [];
+    var titles = $("li.item h2");
+    for(var i=0;i<titles.length;i++) {
+      result.push($(titles[i]).text());
+    }
+    event.reply("result.json", JSON.stringify(result));
+  });
   }
 });
+
+
+// bot.on('message', function(event) {
+//   if (event.message.type = 'text') {
+//   	_japan();
+//     var msg = event.message.text;
+//   //收到文字訊息時，直接把收到的訊息傳回去
+//     event.reply(msg).then(function(data) {
+//     	event.reply('...');
+//       // 傳送訊息成功時，可在此寫程式碼 
+//       console.log(msg);
+//     }).catch(function(error) {
+//       // 傳送訊息失敗時，可在此寫程式碼 
+//       console.log('錯誤產生，錯誤碼：'+error);
+//     });
+//   }
+// });
 
 
 
@@ -65,28 +83,28 @@ var server = app.listen(process.env.PORT || 8080, function() {
 //   });
 // };
 
-function _japan() {
-  // clearTimeout(timer2);
-  request({
-  	url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
-    method: "GET"
-  }, function(error, response, body) {
-    if (error || !body) {
-    	var gg ='GG';
-    	return gg;
-    } else {
-    	var $ = cheerio.load(body);
-        var target = $(".rate-content-cash text-right print_hide");
-        console.log(target[0].children[0].data);
-        var jp = target[0].children[0].data;
-        // var jp ='fuck';
+// function _japan() {
+//   // clearTimeout(timer2);
+//   request({
+//   	url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
+//     method: "GET"
+//   }, function(error, response, body) {
+//     if (error || !body) {
+//     	var gg ='GG';
+//     	return gg;
+//     } else {
+//     	var $ = cheerio.load(body);
+//         var target = $(".rate-content-cash text-right print_hide");
+//         console.log(target[0].children[0].data);
+//         var jp = target[0].children[0].data;
+//         // var jp ='fuck';
         
 
-        bot.push('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
+//         bot.push('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
 
-      // timer2 = setInterval(_japan, 1000);
-    }
-  });
+//       // timer2 = setInterval(_japan, 1000);
+//     }
+//   });
 }
 
 
