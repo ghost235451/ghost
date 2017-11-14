3dcd3a202085d5e03878ef2ba6e9cc3623fc081e
kenny <kenny841126@gmail.com>
2017-11-15 00:37:43 +0800

    z

---
+++ kenny/config/line-bot.js
@@ -0,0 +1,3 @@
+export const channelID = '1493441304'
+export const channelSecret = '44f2a25f46d7100c16726e8940c52167'
+export const channelToken = '7zASQbXqOsguMl45+S3U4cZnyhdrem9AzFwQG2j1wQW4Ab9Jwdfd3S3mQHDqUXwIPwiffFy6VvGkGUPvDhL4pRYCxIpjJ3Dl+81VbUAp2b485s75+AIg7sNQuMQqV/hwNb4fXp5F3SdCsVHUC33vzwdB04t89/1O/w1cDnyilFU='
