c59dcde90977af520a00aeff9c77b616b114587c
kenny <kenny841126@gmail.com>
2017-11-15 01:29:09 +0800

    z

--- kenny/server/index.js
+++ kenny/server/index.js
@@ -20,7 +20,7 @@ const server = require('http').Server(app)
 let bot = LINEBot.create({
   channelID:'1522726717',
   channelSecret:'1d69960dcb17f09bb3bbd5caf820a1c5',
-  channelAccessToken:'channelAccessToken": "/0HWJ3EzlNXylQ3+tC3iDdHm95e+QOhpXKy0bYf49UknQ+qobarTauYCMku/0+xgkhPe6t2MYNnYl0/9KN8hxMdi1CEVuRSQTO9NvBSL9HSDK++01uu5o6SEchXL9fS4NKODAfuLcDCZGG07jse2iQdB04t89/1O/w1cDnyilFU='
+  channelAccessToken:'/0HWJ3EzlNXylQ3+tC3iDdHm95e+QOhpXKy0bYf49UknQ+qobarTauYCMku/0+xgkhPe6t2MYNnYl0/9KN8hxMdi1CEVuRSQTO9NvBSL9HSDK++01uu5o6SEchXL9fS4NKODAfuLcDCZGG07jse2iQdB04t89/1O/w1cDnyilFU='
 }, server)
 
 app.use(bot.webhook('/webhook'))
