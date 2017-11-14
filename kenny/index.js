199dbf214b8ad2a8781bdaf125db701b7c90c404
kenny <kenny841126@gmail.com>
2017-11-15 00:21:15 +0800

    z

--- kenny/index.js
+++ kenny/index.js
@@ -117,7 +117,7 @@ function _japan() {
       // var jp2 = target[0].children[0].data;
       // if (jp > 0) {
       	bot.on('message',function(event){
-      		event.reply( jp );
+      		event.reply(jp);
       	});
         // bot.reply('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
       // }
