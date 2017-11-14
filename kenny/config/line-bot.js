e26cece62630a0794d4bf6ecd1e749fccfd140a0
kenny <kenny841126@gmail.com>
2017-11-15 01:26:02 +0800

    z

--- kenny/config/line-bot.js
+++ kenny/config/line-bot.js
@@ -1,3 +1,3 @@
 export const channelID = '1522726717'
 export const channelSecret = '1d69960dcb17f09bb3bbd5caf820a1c5'
-export const channelToken = 'channelAccessToken": "/0HWJ3EzlNXylQ3+tC3iDdHm95e+QOhpXKy0bYf49UknQ+qobarTauYCMku/0+xgkhPe6t2MYNnYl0/9KN8hxMdi1CEVuRSQTO9NvBSL9HSDK++01uu5o6SEchXL9fS4NKODAfuLcDCZGG07jse2iQdB04t89/1O/w1cDnyilFU='
+export const channelToken = '/0HWJ3EzlNXylQ3+tC3iDdHm95e+QOhpXKy0bYf49UknQ+qobarTauYCMku/0+xgkhPe6t2MYNnYl0/9KN8hxMdi1CEVuRSQTO9NvBSL9HSDK++01uu5o6SEchXL9fS4NKODAfuLcDCZGG07jse2iQdB04t89/1O/w1cDnyilFU='
