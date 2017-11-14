57677faa3b0d6ef304488c9847a584a56bbbbbac
kenny <kenny841126@gmail.com>
2017-11-15 00:45:43 +0800

    z

--- kenny/server/index.js
+++ kenny/server/index.js
@@ -18,9 +18,9 @@ const googleMapsClient = require('@google/maps').createClient({ key: 'AIzaSyDjyh
 let app = express()
 const server = require('http').Server(app)
 let bot = LINEBot.create({
-  channelID:'1522726717',
-  channelSecret:'1d69960dcb17f09bb3bbd5caf820a1c5',
-  channelAccessToken: '/0HWJ3EzlNXylQ3+tC3iDdHm95e+QOhpXKy0bYf49UknQ+qobarTauYCMku/0+xgkhPe6t2MYNnYl0/9KN8hxMdi1CEVuRSQTO9NvBSL9HSDK++01uu5o6SEchXL9fS4NKODAfuLcDCZGG07jse2iQdB04t89/1O/w1cDnyilFU='
+  channelID,
+  channelSecret,
+  channelAccessToken
 }, server)
 
 app.use(bot.webhook('/webhook'))
