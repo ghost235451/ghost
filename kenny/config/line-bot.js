cbf2dabcee42ceaacac01a285e4014668a1ad1d4
kenny <kenny841126@gmail.com>
2017-12-15 11:22:12 +0800

    zz

--- kenny/config/line-bot.js
+++
@@ -1,3 +0,0 @@
-export const channelID = '1522726717'
-export const channelSecret = '1d69960dcb17f09bb3bbd5caf820a1c5'
-export const channelToken = '/0HWJ3EzlNXylQ3+tC3iDdHm95e+QOhpXKy0bYf49UknQ+qobarTauYCMku/0+xgkhPe6t2MYNnYl0/9KN8hxMdi1CEVuRSQTO9NvBSL9HSDK++01uu5o6SEchXL9fS4NKODAfuLcDCZGG07jse2iQdB04t89/1O/w1cDnyilFU='
--- kenny/utils/index.js
+++
@@ -1,4 +0,0 @@
-export const ErrorLogger = (res, reason, message, code) => {
-  console.log('[ErrorLogger]: ' + reason)
-  res.status(code || 500).json({ error: message})
-}
